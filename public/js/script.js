77bd4b06dbd9ec7305a3e6fb33df04c758d64f9e
nikkesan <nikke.san@gmail.com>
2019-11-27 16:47:37 +0000

    Create search function for city search
    Add divs for dropdown menu
    Add classes to form input and form button
    
    Add event-listener and option select function to make weather API call (placeholder)
    Add new search-call.js file - includes search cities and DOM functions
    
    Co-authored by: Reggie <reginaldjbeili@gmail.com>

---
+++ public/js/script.js
@@ -0,0 +1,23 @@
+const cityBox = document.querySelector('input[name="cityBox"]');
+const searchButton = document.querySelector("#search-button");
+
+// if city is typed in and search button is selected, make weather API request
+searchButton.addEventListener("click", () => {
+  event.preventDefault();
+  if (cityBox.value) {
+    // weather API call goes here
+  }
+});
+
+// if city is selected from dropdown menu, make weather API request
+const optionSelect = () => {
+  if (cityBox.value) {
+    // weather API call goes here
+  }
+};
+
+const chooseOption = event => {
+  let optionText = event.currentTarget.textContent;
+  siteBox.value = optionText;
+  optionSelect();
+};

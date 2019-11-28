a260e3b8156d18d6587161ce5209e3977bf6176c
Roshan Thaabith <roshlarosh@gmail.com>
2019-11-28 12:20:46 +0000

    add travis file

--- test/server.test.js
+++ test/server.test.js
@@ -16,7 +16,7 @@ test("Home route", t => {
   supertest(router)
     .get("/")
     .expect(200)
-    // .expect("Content-Type", /html/)
+    .expect("Content-Type", /html/)
     .end((err, res) => {
       t.error(err);
       t.equal(res.text, "Hello", "response should contain 'Hello'");

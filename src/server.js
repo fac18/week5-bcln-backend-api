3aaa7a4a8a8d8e0f5514c2b8065407e186447aee
Roshan Thaabith <roshlarosh@gmail.com>
2019-11-29 12:05:28 +0000

    change port to uppercase

--- src/server.js
+++ src/server.js
@@ -1,9 +1,9 @@
 const http = require("http");
 const router = require("./router.js");
-const port = process.env.PORT || 8888;
+const PORT = process.env.PORT || 8888;
 
 const server = http.createServer(router);
 
-server.listen(port, () => {
+server.listen(PORT, () => {
   console.log("The server is listening on 8888. Ready to accept requests");
 });
